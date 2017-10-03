a6a3e45e2c76813835d8d55ece6244886a2867c5
POLAND\ASuzanovich <asuzanovich@luxoft.com>
2017-10-03 13:43:56 +0200

    add reverse LL by groups

--- py/reverse_LL_in_groups.py
+++ py/reverse_LL_in_groups.py
@@ -11,7 +11,13 @@ you need to write your solution in the form of Function(s) only.
 Driver Code to call/invoke your function would be added by GfG's Online Judge.'''
 
 
-"""Return reference of new head of the reverse linked list
+"""
+Given a linked list, write a function to reverse every k nodes 
+(where k is an input to the function).If a linked list is given as 
+1->2->3->4->5->6->7->8->NULL and k = 3 then output will be 3->2->1->6->5->4->8->7->NULL.
+
+----------------------
+Return reference of new head of the reverse linked list
   The input list will have at least one element
   Node is defined as
 class Node:
@@ -20,8 +26,9 @@ class Node:
 		self.next = None
   This is method only submission.
   You only need to complete the method.
+  
+  
 """
-
 
 def reverse(head, k):
     # Code here
@@ -29,9 +36,10 @@ def reverse(head, k):
     i = 0
     cur_node = head
     new_head = None
-    prev_group_head = None
-    group_tail = head
+    prev_group_tail = head
+    group_tail = None
     prev_node = None
+    first_group = True
     while cur_node:
         if i < k:
             next_node = cur_node.next
@@ -41,15 +49,17 @@ def reverse(head, k):
         
         i += 1
         
-        if i == k:
-            if not new_head:
+        if i == k or not cur_node:
+            if first_group:
                 new_head = prev_node
             
-            if prev_group_head:
-                group_tail.next = prev_group_head
-                group_tail = cur_node
+            if not first_group:
+                prev_group_tail.next = prev_node
+                prev_group_tail = group_tail
                 
-            prev_group_head = prev_node
+            group_tail = cur_node
+            first_group = False
+            
             prev_node = None
             
             i = 0
